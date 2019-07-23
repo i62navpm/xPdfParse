3d0c6b67d7182942c8cfb1683425c571f025551a
m.navarro <m.navarro@payvision.com>
2019-07-23 22:07:36 +0200

    feat: added two new parsers

--- packages/extractInfo/src/index.js
+++ packages/extractInfo/src/index.js
@@ -1,11 +1,23 @@
 const parseNormalList = require('./normalList')
+const parseBilingualList = require('./bilingualList')
+// const parseAssignmentList = require('./assignmentList')
 const consola = require('consola')
   .withDefaults({ badge: true })
   .withTag('extractInfo')
 
-try {
-  consola.log('Parsing "normalList"')
-  parseNormalList()
-} catch (err) {
-  console.error(err)
+async function init() {
+  try {
+    consola.log('Parsing "normalList"')
+    await parseNormalList()
+
+    consola.log('Parsing "bilingualList"')
+    await parseBilingualList()
+
+    // consola.log('Parsing "assignmentList"')
+    // await parseAssignmentList()
+  } catch (err) {
+    consola.error(err)
+  }
 }
+
+init()
