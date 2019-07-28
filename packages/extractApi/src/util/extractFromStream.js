6d2d0e560c5d5f9658abc8bb3a5066efed019547
m.navarro <m.navarro@payvision.com>
2019-07-28 19:40:21 +0200

    feat: extract inline data

--- packages/extractApi/src/util/extractFromStream.js
+++ packages/extractApi/src/util/extractFromStream.js
@@ -3,7 +3,10 @@ const specialtyHelper = require('../helpers/specialty')
 const oppositorHelper = require('../helpers/oppositor')
 const fixLineBreak = require('../helpers/fixLineBreak')
 
-module.exports = ({ list, specialty }, { debug = false } = {}) => {
+module.exports = (
+  { list, specialty },
+  { debug = false, specialtyInline = false } = {}
+) => {
   const sp = specialtyHelper({ list, specialty })
   const op = oppositorHelper({ list, specialty })
   const fixLB = fixLineBreak(list)
@@ -16,19 +19,36 @@ module.exports = ({ list, specialty }, { debug = false } = {}) => {
       }
 
       try {
-        if (fixLB.checkIfChunkTemporarily()) {
-          chunk = fixLB.mergeChunkWithTemporarily(chunk)
-          this.push(JSON.stringify(op.getOppositor(chunk)))
-        } else if (sp.isSpecialty(chunk)) {
-          this.push(
-            JSON.stringify(specialty ? { specialty } : sp.getSpecialty(chunk))
-          )
-        } else if (op.isOppositor(chunk)) {
-          if (fixLB.checkNeedToFixLineBreak(chunk)) {
-            fixLB.saveChunkTemporarily(chunk)
-            return callback()
+        if (specialtyInline) {
+          if (fixLB.checkIfChunkTemporarily()) {
+            chunk = fixLB.mergeChunkWithTemporarily(chunk)
+            const { specialty, ...oppositor } = op.getOppositor(chunk)
+            this.push(JSON.stringify({ specialty }))
+            this.push(JSON.stringify(oppositor))
+          } else if (op.isOppositor(chunk)) {
+            if (fixLB.checkNeedToFixLineBreak(chunk)) {
+              fixLB.saveChunkTemporarily(chunk)
+              return callback()
+            }
+            const { specialty, ...oppositor } = op.getOppositor(chunk)
+            this.push(JSON.stringify({ specialty }))
+            this.push(JSON.stringify(oppositor))
           }
-          this.push(JSON.stringify(op.getOppositor(chunk)))
+        } else {
+          if (fixLB.checkIfChunkTemporarily()) {
+            chunk = fixLB.mergeChunkWithTemporarily(chunk)
+            this.push(JSON.stringify(op.getOppositor(chunk)))
+          } else if (sp.isSpecialty(chunk)) {
+            this.push(
+              JSON.stringify(specialty ? { specialty } : sp.getSpecialty(chunk))
+            )
+          } else if (op.isOppositor(chunk)) {
+            if (fixLB.checkNeedToFixLineBreak(chunk)) {
+              fixLB.saveChunkTemporarily(chunk)
+              return callback()
+            }
+            this.push(JSON.stringify(op.getOppositor(chunk)))
+          }
         }
         callback()
       } catch (err) {
