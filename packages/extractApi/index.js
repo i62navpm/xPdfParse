ff28536db6016fd0605d3406c36e8ab286e7f1eb
m.navarro <m.navarro@payvision.com>
2019-07-29 08:12:41 +0200

    refactor: change logic from main source

---
+++ packages/extractApi/index.js
@@ -0,0 +1,89 @@
+const { extractApi } = require('./src')
+const consola = require('consola')
+  .withDefaults({ badge: true })
+  .withTag('extractApi')
+
+const normalList = [
+  {
+    list: 'normalList',
+    specialty: 'AL',
+  },
+  {
+    list: 'normalList',
+    specialty: 'EF',
+  },
+  {
+    list: 'normalList',
+    specialty: 'EI',
+  },
+  {
+    list: 'normalList',
+    specialty: 'FI',
+  },
+  {
+    list: 'normalList',
+    specialty: 'FR',
+  },
+  {
+    list: 'normalList',
+    specialty: 'MU',
+  },
+  {
+    list: 'normalList',
+    specialty: 'PRI',
+  },
+  {
+    list: 'normalList',
+    specialty: 'PT',
+  },
+]
+
+const bilingualList = [
+  {
+    list: 'bilingualList',
+  },
+]
+
+const voluntaryList = [
+  {
+    list: 'voluntaryList',
+  },
+]
+
+async function init() {
+  try {
+    for (const list of normalList) {
+      await extractApi
+        .from(list, { debug: false })
+        .extract()
+        .normalize()
+        .save()
+    }
+  } catch (err) {
+    consola.error(`[NormalList]${err}`)
+  }
+  try {
+    for (const list of bilingualList) {
+      await extractApi
+        .from(list, { debug: false })
+        .extract()
+        .normalize()
+        .save()
+    }
+  } catch (err) {
+    consola.error(`[BilingualList]${err}`)
+  }
+  try {
+    for (const list of voluntaryList) {
+      await extractApi
+        .from(list, { debug: false, specialtyInline: true })
+        .extract()
+        .normalize()
+        .save()
+    }
+  } catch (err) {
+    consola.error(`[VoluntaryList]${err}`)
+  }
+}
+
+init()
